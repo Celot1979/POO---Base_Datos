f2ab3596b0ee7352b9b9bca59d905eb333d721b8
Celot1979 <zata_k@hotmail.com>
2026-01-29 13:27:54 +0100

    Creación Tablas realcionadas

--- 02_Introduccion_SQL/taller_sql.py
+++ 02_Introduccion_SQL/taller_sql.py
@@ -22,7 +22,11 @@ print("\n1. Creando Tablas...")
 # - Nombre (Texto, Not Null)
 # - Ciudad (Texto)
 sql_crear_clientes = """
--- ESCRIBE TU CÓDIGO AQUÍ (borra esto)
+CREATE TABLE CLIENTES (
+    ID_Cliente INTEGER PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Ciudad VARCHAR(50)
+);
 """
 
 try:
@@ -38,11 +42,23 @@ print("\n2. Insertando Datos...")
 
 # TAREA: Inserta un cliente: "Empresa A" de "Gijón"
 sql_insertar_1 = """
+INSERT INTO CLIENTES(Nombre, Ciudad) VALUES ('Empresa A', 'Gijón');
+"""
+
+# TAREA EXTRA: INSERTAR MÁS DATOS
+# En SQL, para insertar varios a la vez se separan por comas:
+sql_insertar_varios = """
+INSERT INTO CLIENTES (Nombre, Ciudad) VALUES 
+    ('Empresa B', 'Avilés'),
+    ('Empresa C', 'Sevilla'),
+    ('Empresa D', 'Sevilla'),
+    ('Empresa E', 'Vigo');
 """
 
 try:
     cursor.execute(sql_insertar_1)
-    print("✅ Cliente 1 insertado.")
+    cursor.execute(sql_insertar_varios) # <-- DESCOMENTADO PARA QUE FUNCIONE
+    print("✅ Clientes insertados.")
 except Exception as e:
     print(f"❌ Error insertando: {e}")
 
@@ -51,15 +67,27 @@ except Exception as e:
 # ==========================================
 print("\n3. Consultando Datos...")
 
-# TAREA: Selecciona TODO de la tabla CLIENTES
-sql_select_todos = "SELECT * FROM CLIENTES" # Te regalo esta de ejemplo
+# TAREA 1: Selecciona TODO de la tabla CLIENTES
+sql_select_todos = "SELECT * FROM CLIENTES"
 
 print(f"Ejecutando: {sql_select_todos}")
 cursor.execute(sql_select_todos)
 resultados = cursor.fetchall()
 
-print("--- Resultados ---")
+print("--- Todos los Clientes ---")
 for fila in resultados:
+    print(fila)
+
+# TAREA 2: CONSULTA CON FILTRO (WHERE)
+# Instrucciones: Selecciona solo los clientes de una ciudad específica.
+sql_where = """
+SELECT Ciudad From Clientes WHERE Ciudad = "Sevilla";
+"""
+
+print("\n--- Resultados del Filtro (Pendiente) ---")
+cursor.execute(sql_where)      # <-- DESCOMENTAR CUANDO ESTÉ LISTO EL WHERE
+resultados_filtro = cursor.fetchall()
+for fila in resultados_filtro:
     print(fila)
 
 # Cierre
---
+++ 02_Introduccion_SQL/taller_sql_2.py
@@ -0,0 +1,113 @@
+"""
+TALLER SQL 2: ESTRUCTURA DESDE CERO
+-----------------------------------
+Objetivo: Crear una estructura modular separando la lógica de la base de datos.
+Este archivo actuará como nuestro "Módulo de Base de Datos" (Modelo/Infraestructura).
+
+PASOS:
+1. Importar librería sqlite3
+2. Definir nombre de la BB.DD.
+3. Crear función para conectar
+4. Crear tablas
+...
+"""
+# ¡TODO TUYO! Empieza importando y definiendo la conexión...
+import sqlite3
+import os
+# ==========================================
+# 3. TABLA TIENDA
+# ==========================================
+DB_Name = "TIENDA.db"
+# Esto es para si la base de datos ya existe la borre.
+if os.path.exists(DB_Name):
+    os.remove(DB_Name)
+
+conexion = sqlite3.connect(DB_Name)
+cursor = conexion.cursor()
+
+Tabla_Clientes = """
+CREATE TABLE CLIENTES (
+    ID_Cliente INTEGER PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Email VARCHAR(100),
+    Ciudad VARCHAR(50)
+);
+"""
+try:
+    cursor.execute(Tabla_Clientes)
+except Exception as e:
+     print(f"❌ Error creando CLIENTES: {e}")
+
+# ==========================================
+# 3. TABLA PEDIDOS 
+# ==========================================
+
+DB_Name_Pedidos = "PEDIDOS.db"
+
+Tabla_Pedidos= """
+CREATE TABLE PEDIDOS (
+    ID_Pedido INTEGER PRIMARY KEY,
+    Fecha DATE,
+    ID_Cliente INT,
+    
+    FOREIGN KEY ( ID_Cliente) REFERENCES CLIENTES(ID_Cliente )
+    
+);
+"""
+
+try:
+    cursor.execute(Tabla_Pedidos)
+except Exception as e:
+     print(f"❌ Error creando CLIENTES: {e}")
+
+# ==========================================
+# 3. TABLA PRODUCTOS
+# ==========================================
+# Reto Final DDL: ID_Producto (PK), Nombre, Precio (REAL/FLOAT), Stock (INTEGER)
+Tabla_Productos = """
+     CREATE TABLE PRODUCTOS(
+        ID_Producto INTEGER PRIMARY KEY,
+        Nombre VARCHAR (50),
+        Precio FLOAT,
+        Stock INTEGER
+     );
+
+"""
+cursor.execute(Tabla_Productos)
+
+# ==========================================
+# 4. INSERTAR DATOS
+# ==========================================
+# Misión 1: Insertar 3 Clientes (Recuerda: Nombre, Email, Ciudad)
+sql_insertar_clientes = """
+INSERT INTO CLIENTES (Nombre, Email, Ciudad) VALUES
+    ('Daniel', 'daniel@email.com', 'Avilés'),
+    ('Sandra', 'sandra@email.com', 'Oviedo'),
+    ('Rafaela', 'rafaela@email.com', 'Cudillero');
+"""
+cursor.execute(sql_insertar_clientes)
+
+# Misión 2: Insertar 3 Productos (Nombre, Precio, Stock)
+sql_insertar_productos = """
+INSERT INTO PRODUCTOS (Nombre, Precio, Stock) VALUES
+    ('Plátano', 1.25, 600),
+    ('Sandia', 8.0, 300),
+    ('Manzanas', 2.58, 500);
+"""
+cursor.execute(sql_insertar_productos)
+
+# Misión 3: Insertar 2 Pedidos (Fecha, ID_Cliente)
+# ¡OJO! El ID_Cliente tiene que existir en la tabla de arriba (ej. 1, 2 o 3)
+# Formato Fecha: 'YYYY-MM-DD' (ej. '2024-01-30')
+sql_insertar_pedidos = """
+INSERT INTO PEDIDOS(Fecha, ID_Cliente) VALUES
+    ('2021-03-21', 1),
+    ('2025-02-21', 3);
+"""
+cursor.execute(sql_insertar_pedidos)
+
+
+conexion.commit()
+print("¡Base de Datos generada con éxito!")
+print("Datos insertados correctamente") # <-- Descomentar al final
+conexion.close()
